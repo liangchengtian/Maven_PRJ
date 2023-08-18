fab3664dabb46397ae47b8eaa5d22008942f70a8
liangchengtian <1101157577@qq.com>
2023-08-18 10:33:35 +0800

    save...

---
+++ fiveset_springboot/src/main/java/com/fiveset/FivesetSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fiveset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivesetSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivesetSpringbootApplication.class, args);
+	}
+
+}
